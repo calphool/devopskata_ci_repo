f33f981833cb36fb3e2bedd8d22a455ea360c47b
calphool <jrounceville@hotmail.com>
2016-07-27 16:13:36 -0500

    integrate number converter class

--- src/main/java/com/rounceville/SimpleWebAppServlet.java
+++ src/main/java/com/rounceville/SimpleWebAppServlet.java
@@ -7,15 +7,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.rounceville.EnglishNumberToWords;
 
 public class SimpleWebAppServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                int i = 14562;
 		PrintWriter out = resp.getWriter();
 		resp.setContentType("text/html");
 		
 		out.println("Hello World!");
                 out.println("<hr/>");
                 out.println("Now is the time for all good men to come to the aid of their country.");
+                out.println("<hr/>");
+                out.println(i + " = " + EnglishNumberToWords.convert(i));
 		out.close();
 	}
 }

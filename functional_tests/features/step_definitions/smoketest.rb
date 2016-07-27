36c871bc495a90503162e501f18999754cd0597f
Cloud User <ec2-user@jenkins.rounceville.com>
2016-07-27 02:54:45 -0400

    fixed tests

--- functional_tests/features/step_definitions/smoketest.rb
+++ functional_tests/features/step_definitions/smoketest.rb
@@ -1,11 +1,12 @@
 Given(/^the user has opened a browser$/) do
-  puts "...browser opened by env.rb..."
+  #puts "...browser opened by env.rb..."
 end
 
 When(/^the user goes to our URL$/) do
+  puts "navigating to #{ENV['APPURL']}"
   @browser.goto ENV['APPURL']
 end
 
 Then(/^he will see "(.*?)"$/) do |arg1|
-  @browser.text.include? "#{arg1}"
+  Watir::Wait.until(timeout=12,message="failed waiting for text to appear") { @browser.text.include? "#{arg1}" }
 end
--- functional_tests/features/support/env.rb
+++ functional_tests/features/support/env.rb
@@ -10,22 +10,29 @@ else
 end
 
 
-browser = Watir::Browser.new :firefox
-
-Before do
-   @browser = browser
-end
-
 if ENV['HEADLESS']
-  require 'headless'
+  puts "Headless mode"
   headless = Headless.new
   headless.start
+  puts "creating browser"
+  browser = Watir::Browser.new :firefox
+
   at_exit do
-    headless.destroy
+    puts "closing browser"
     browser.close
+    puts "destroying headless object"
+    headless.destroy
   end
 else
+  puts "creating browser"
+  browser = Watir::Browser.new :firefox
   at_exit do
+    puts "closing browser"
     browser.close
   end
+end
+
+Before do
+#   puts "making @browser global"
+   @browser = browser
 end

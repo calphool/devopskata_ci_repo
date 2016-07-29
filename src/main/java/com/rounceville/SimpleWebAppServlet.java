50e76ae26a8a5f91aae7f33afd4561ee1d219d0a
calphool <jrounceville@hotmail.com>
2016-07-29 01:03:33 -0500

    add post behaviors

--- src/main/java/com/rounceville/SimpleWebAppServlet.java
+++ src/main/java/com/rounceville/SimpleWebAppServlet.java
@@ -20,6 +20,24 @@ public class SimpleWebAppServlet extends HttpServlet {
                 out.println("Now is the time for all good men to come to the aid of their country.");
                 out.println("<hr/>");
                 out.println(i + " = " + EnglishNumberToWords.convert(i));
+                out.println("<hr/>");
+                out.println("<form action='.' method='post'>\n");
+                out.println("Type a number:\n");
+                out.println("<input type=text name='thenumber'>\n");
+                out.println("<input type=submit name=submit value=submit>\n");
+                out.println("</form>");
 		out.close();
 	}
+
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                String thenumber = request.getParameter("thenumber");
+		PrintWriter out = resp.getWriter();
+		
+                resp.setContentType("text/html");
+
+                out.println("You sent the value: " + thenumber + "<br/>");
+                out.println("Which means: " + EnglishNumberToWords.convert(Integer.parse(thenumber)));
+
+                out.println("<br/><br/><a href='.'>Back</a>");
+        }
 }
